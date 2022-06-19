ea2ea425f81e3e535055d404e1c4f4f9e06efa32
Kamil <kamgre7@gmail.com>
2022-06-19 22:39:37 +0200

    init comit - react with chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  theme,
+} from '@chakra-ui/react';
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl" />
+  </ChakraProvider>
+);
---
+++ src/test-utils.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+
+// eslint-disable-next-line react/require-default-props
+const AllProviders = ({ children }: { children?: React.ReactNode }) => (
+  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+);
+
+// eslint-disable-next-line max-len
+const customRender = (ui: React.ReactElement, options?: RenderOptions) => render(ui, { wrapper: AllProviders, ...options });
+
+export { customRender as render };

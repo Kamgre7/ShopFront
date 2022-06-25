bced522f01b5beaa182a30aa917e56bdbf86ee57
Kamil <kamgre7@gmail.com>
2022-06-25 17:50:24 +0200

    Added upload Product form with image to backend

--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -22,14 +22,24 @@ export const AddProductForm = () => {
       price: 0,
       sku: '',
       category: '',
-      img: '',
+      photo: '',
     },
     onSubmit: async (values) => {
       try {
+        const formData = new FormData();
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [key, value] of Object.entries(values)) {
+          if (key !== 'photo') {
+            formData.append(key, String(value));
+          } else {
+            formData.append('photo', values.photo);
+          }
+        }
+
         await fetch('http://localhost:3001/', {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(values),
+          body: formData,
         });
       } catch (err) {
         console.error(err);
@@ -40,7 +50,7 @@ export const AddProductForm = () => {
   return (
     <Flex bg="gray.100" align="center" justify="center" h="100vh">
       <Box bg="white" p={6} rounded="md">
-        <form onSubmit={formik.handleSubmit}>
+        <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
           <VStack spacing={4} align="flex-start">
             <FormControl>
               <FormLabel htmlFor="name">Name</FormLabel>
@@ -125,17 +135,20 @@ export const AddProductForm = () => {
               </Select>
             </FormControl>
             <FormControl>
-              <FormLabel htmlFor="img">Image</FormLabel>
+              <FormLabel htmlFor="photo">Image</FormLabel>
               <Input
-                id="img"
-                name="img"
+                id="photo"
+                name="photo"
                 type="file"
                 variant="filled"
-                onChange={formik.handleChange}
-                value={formik.values.img}
+                onChange={(e) => {
+                  // @ts-ignore: Object is possibly 'null'.
+                  formik.setFieldValue('photo', e.currentTarget.files[0]);
+                }}
                 bg="white"
                 border="2px"
                 borderColor="gray.100"
+                accept="image/*"
               />
             </FormControl>
             <Button type="submit" colorScheme="purple" width="full">

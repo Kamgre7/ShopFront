f6473a21010f2833dfed3cca67ef8079159eccfe
Kamil <kamgre7@gmail.com>
2022-07-07 18:23:18 +0200

    removed console.log from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { Box, Center } from '@chakra-ui/react';
 import { NavigationBar } from './NavigationBar';
 
-export const Header = () => {
-  console.log('asdsdad');
-  return (
-    <Center>
-      <Box p={5} w="50vw" mt="5px" textAlign="center">
-        <NavigationBar />
-      </Box>
-    </Center>
-  );
-};
+export const Header = () => (
+  <Center>
+    <Box p={5} w="50vw" mt="5px" textAlign="center">
+      <NavigationBar />
+    </Box>
+  </Center>
+);
